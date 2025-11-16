bd0b5b4aa1ca14aa4fc791890fda4394701ee6d5
JISIL 444 <jisil444@JISILs-MacBook-Air.local>
2025-11-16 11:19:36 +0530

    add vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Explicitly set base path
   plugins: [
     tailwindcss(),
-    react()],
+    react()
+  ],
 })
